31f1f65de792036aab7fad2dbc3a2f8bb9ba4cc2
joseph mwanzia <makamwanzia@gmail.com>
2023-03-29 10:09:49 +0300

    Edited put route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/heroes', to: 'heroes#show'
   get '/powers', to: 'powers#index'
   get '/powers', to: 'powers#show'
-  put '/powers', to: 'powers#update'
+  put '/powers/:id', to: 'powers#update'
   post '/hero_powers', to: 'hero_powers#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
