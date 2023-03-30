6dd4b9e129c09395762f555b4ffc26f6f8eddf69
joseph mwanzia <makamwanzia@gmail.com>
2023-03-31 01:43:51 +0300

    Edited line 2 & 3 for id routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/heroes', to: 'heroes#index'
-  get '/heroes', to: 'heroes#show'
+  get '/heroes/:id', to: 'heroes#show'
   get '/powers', to: 'powers#index'
-  get '/powers', to: 'powers#show'
+  get '/powers/:id', to: 'powers#show'
   put '/powers/:id', to: 'powers#update'
   post '/hero_powers', to: 'hero_powers#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
