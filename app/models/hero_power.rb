b19abf7fdf7dfbeefe797140b51a8087eb5307f9
joseph mwanzia <makamwanzia@gmail.com>
2023-03-31 01:45:23 +0300

    Formating changes

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,5 +1,5 @@
 class HeroPower < ApplicationRecord
     belongs_to :hero
-    validates :strength, inclusion: { in: %w(Strong Weak Average)} 
     belongs_to :power
+    validates :strength, inclusion: { in: %w(Strong Weak Average)} 
 end
