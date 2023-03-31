710d1b2248ac8919dd1daad21733b8abf56610e0
joseph mwanzia <makamwanzia@gmail.com>
2023-03-31 03:08:39 +0300

    Created various serializers with associations

--- app/serializers/heropower_new_power_serializer.rb
+++ app/serializers/heropower_new_power_serializer.rb
@@ -1,3 +1,6 @@
 class HeropowerNewPowerSerializer < ActiveModel::Serializer
   attributes :id, :name, :superpower
+
+  belongs_to :hero
+  belongs_to :power
 end
---
+++ app/serializers/powers_serializer.rb
@@ -0,0 +1,6 @@
+class PowersSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description
+
+  has_many :hero_powers
+  has_many :heros, through: :hero_powers
+end
