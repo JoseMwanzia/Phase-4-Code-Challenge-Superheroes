0fe30d63d97911a48e8924838939b81675e4aaf6
joseph mwanzia <makamwanzia@gmail.com>
2023-03-31 03:09:05 +0300

    Updated migrations and schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,23 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_28_205524) do
   create_table "hero_powers", force: :cascade do |t|
+    t.integer "hero_id"
+    t.integer "power_id"
+    t.string "strength"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
